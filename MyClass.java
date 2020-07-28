ef9f8ea1f970987fac4f6858043754e1cd47c15c
priyadarshanvj <63783259+priyadarshanvj@users.noreply.github.com>
2020-07-28 17:48:24 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+class demo2
+{
+  public void fun(int a,int b)
+     { 
+         System.out.print(a+b);
+     }
+}
+public class MyClass
+{ 
+   public static void main(String args[])
+     { 
+        demo2 obj=new demo2();
+        obj.fun(5,10);
+     }
+}                
