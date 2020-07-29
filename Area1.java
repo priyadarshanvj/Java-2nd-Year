904c29625f22922d28367293eb541ea99de2f438
priyadarshanvj <63783259+priyadarshanvj@users.noreply.github.com>
2020-07-29 22:51:24 +0530

    Add files via upload

---
+++ Area1.java
@@ -0,0 +1,17 @@
+class pri
+{
+	public void area()
+	{
+	   int s=5;
+	   int a=s*s;
+	   System.out.print(a);
+	}
+}
+public class Area1
+{
+	public static void main(String args[])
+	{
+		pri obj=new pri();
+                obj.area();
+	}
+}
